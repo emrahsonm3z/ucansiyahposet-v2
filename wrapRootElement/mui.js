6e2d6543e7314570d7f77de04d7ed83972e6eeee
Emrah Sönmez <emrahsonm3z@gmail.com>
2019-03-12 16:31:01 +0300

    chane mui.js

--- wrapRootElement/mui.js
+++ wrapRootElement/mui.js
@@ -12,11 +12,15 @@ export const getSheetsRegistry = () => context && context.sheetsRegistry;
 function createPageContext() {
   return {
     theme,
+    // This is needed in order to deduplicate the injection of CSS in the page.
     sheetsManager: new Map(),
+    // This is needed in order to inject the critical CSS.
     sheetsRegistry: new SheetsRegistry(),
-    generateClassName: createGenerateClassName()
+    // The standard class name generator.
+    generateClassName: createGenerateClassName(),
   };
 }
+
 
 function getPageContext() {
   let context;
